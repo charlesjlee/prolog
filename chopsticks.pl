e20cffec95c401a30b6d8e232b0d92a23b87a323
Charles Lee <charleslee592@gmail.com>
2020-06-05 11:04:34 -0400

    add chopsticks.pl

---
+++ chopsticks.pl
@@ -0,0 +1,71 @@
+/*
+get_next(hand(1,1,1,1,bottom), X, []).
+setof(X, get_next(hand(1,1,2,0,bottom),X,[]), Xs).
+
+forced_win(hand(0,0,1,2,bottom), []).
+forced_win(hand(3,0,1,2,bottom), []).
+forced_win(hand(0,3,1,1,bottom), []).
+
+forced_win(hand(1,1,1,1,bottom), []).
+
+forced_win(hand(2,1,1,1,top),[]).
+
+forced_win(hand(2, 1, 3, 1, bottom),[]).
+forced_win(hand(2, 1, 1, 2, bottom),[]).
+forced_win(hand(2, 1, 2, 1, bottom),[]).
+forced_win(hand(2, 1, 1, 3, bottom),[]).
+*/
+
+tap(From, To, ToNew) :-
+	From \= 0,
+	Total is From + To,
+	ToNew is mod(Total, 5).
+
+splits(0, 4, 2).
+splits(4, 0, 2).
+splits(0, 2, 1).
+splits(2, 0, 1).
+
+lost(0, 0).
+
+% get_next(hand(TopLeft,TopRight,BottomLeft,BottomRight,Turn), Next, Seen)
+% calculates the Next unSeen state given it is Turn's move
+get_next(hand(TL,TR,BL,BR,bottom), Next, Seen) :- tap(BL,TL,TL_New), Next=hand(TL_New,TR,BL,BR,top), \+member(Next,Seen).
+get_next(hand(TL,TR,BL,BR,bottom), Next, Seen) :- tap(BR,TR,TR_New), Next=hand(TL,TR_New,BL,BR,top), \+member(Next,Seen).
+get_next(hand(TL,TR,BL,BR,bottom), Next, Seen) :- tap(BL,TR,TR_New), Next=hand(TL,TR_New,BL,BR,top), \+member(Next,Seen).
+get_next(hand(TL,TR,BL,BR,bottom), Next, Seen) :- tap(BR,TL,TL_New), Next=hand(TL_New,TR,BL,BR,top), \+member(Next,Seen).
+get_next(hand(TL,TR,BL,BR,top), Next, Seen) :- tap(TL,BL,BL_New), Next=hand(TL,TR,BL_New,BR,bottom), \+member(Next,Seen).
+get_next(hand(TL,TR,BL,BR,top), Next, Seen) :- tap(TR,BR,BR_New), Next=hand(TL,TR,BL,BR_New,bottom), \+member(Next,Seen).
+get_next(hand(TL,TR,BL,BR,top), Next, Seen) :- tap(TR,BL,BL_New), Next=hand(TL,TR,BL_New,BR,bottom), \+member(Next,Seen).
+get_next(hand(TL,TR,BL,BR,top), Next, Seen) :- tap(TL,BR,BR_New), Next=hand(TL,TR,BL,BR_New,bottom), \+member(Next,Seen).
+
+get_next(hand(TL,TR,BL,BR,bottom), Next, Seen) :- splits(BL,BR,S), Next=hand(TL,TR,S,S,top), \+member(Next,Seen).
+get_next(hand(TL,TR,BL,BR,top), Next, Seen) :- splits(TL,TR,S), Next=hand(S,S,BL,BR,bottom), \+member(Next,Seen).
+
+% current player already won
+forced_win(hand(TL,TR,_,_,bottom), _) :- lost(TL,TR), !.
+forced_win(hand(_,_,BL,BR,top), _) :- lost(BL, BR), !.
+
+% current player can win in a single move
+forced_win(hand(TL,TR,BL,BR,bottom), Seen) :-
+	\+ lost(BL, BR),
+	get_next(hand(TL,TR,BL,BR,bottom), hand(TL_2,TR_2,BL,BR,top), Seen),
+	lost(TL_2, TR_2),
+	!.
+forced_win(hand(TL,TR,BL,BR,top), Seen) :-
+	\+ lost(TL, TR),
+	get_next(hand(TL,TR,BL,BR,top), hand(TL,TR,BL_2,BR_2,bottom), Seen),
+	lost(BL_2, BR_2),
+	!.
+
+% current player has a branch that always ends in a win
+forced_win(H1, Seen) :-
+	% can the current player choose a move...
+	get_next(H1, H2, Seen),
+	append(Seen, H1, NewSeen),
+
+	% s.t. all of the opponent's possible moves result in a forced win for the current player?
+	bagof(H3, get_next(H2, H3, NewSeen), Hs),
+	forall(member(H, Hs), forced_win(H, NewSeen)),
+	format('winning move: ~w~n', H2), % the winning move
+	!.
